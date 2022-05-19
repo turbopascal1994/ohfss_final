5182b377ff537b3e76cb1a0a6d5697e5f8b40657
Vsevolod <98434082+turbopascal1994@users.noreply.github.com>
2022-05-19 18:02:02 +0300

    Update CreateStartSCALLOP.m

--- CreateStartSCALLOP.m
+++ CreateStartSCALLOP.m
@@ -1,9 +1,8 @@
 function NewSequence = CreateStartSCALLOP(w01, wt, w, M, AmpThreshold)
 Tt = 2*pi/wt;
 LenImp = (M - 1)*Tt;
-% Строим импульсы
+% РЎС‚СЂРѕРёРј РёРјРїСѓР»СЊСЃС‹
 b = w;
-%M = floor(LenImp/(2*pi)*wt);
 NewSequence = zeros(1,M);
 SequenceString = '';
 NewSeqElement = '';
@@ -13,19 +12,19 @@ while b <= LenImp
     iSeq = 0;
     a = i*Tt;
     b = i*Tt + w;
-    % Положительные импульсы
+    % РџРѕР»РѕР¶РёС‚РµР»СЊРЅС‹Рµ РёРјРїСѓР»СЊСЃС‹
     if sin(w01*a) >= AmpThreshold
         NewSequence(1,(j+1)) = 1;
         NewSeqElement = '1';
         iSeq = 1;
     end
-    % Отрицательные импульсы
+    % РћС‚СЂРёС†Р°С‚РµР»СЊРЅС‹Рµ РёРјРїСѓР»СЊСЃС‹
     if sin(w01*a) <= -AmpThreshold
         NewSequence(1,(j+1)) = -1;
         NewSeqElement = '-1';
         iSeq = 1;
     end
-    % Нули
+    % РќСѓР»Рё
     if iSeq == 0
         NewSequence(1,(j+1)) = 0;
         NewSeqElement = '0';
@@ -34,7 +33,5 @@ while b <= LenImp
     i = i + 1;
     j = j + 1;
 end
-%disp('Стартовая последовательность: ');
-%disp(SequenceString);
 end
 
